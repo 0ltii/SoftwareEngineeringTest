bc88406e224c124b87e953210a802fc706ce7e4a
0ltii <oltiabazi666@gmail.com>
2025-01-11 12:47:24 +0100

    Initial commit

---
+++ src/main/java/dev/oltiabazi/softwareengineeringtest/SoftwareEngineeringTestApplication.java
@@ -0,0 +1,13 @@
+package dev.oltiabazi.softwareengineeringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareEngineeringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareEngineeringTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/oltiabazi/softwareengineeringtest/SoftwareEngineeringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.oltiabazi.softwareengineeringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareEngineeringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
